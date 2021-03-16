331bf42391a350f02f0dfe290ec4f67837cb1a4b
Triston <tristonherbst97@gmail.com>
2021-03-16 16:56:29 -0500

    commit tree d56703456207d6464ea28173c7afe44229e520f1

--- app.rb
+++ app.rb
@@ -2,4 +2,21 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+    get '/' do
+        erb :index
+    end
+
+    get '/new' do
+        @puppy = Puppy.new(params[:name],params[:breed],params[:age])
+
+       erb :create_puppy 
+    end
+
+    post '/puppy' do
+        # binding.pry
+        erb :display_puppy
+    
+    end
+
+
 end
